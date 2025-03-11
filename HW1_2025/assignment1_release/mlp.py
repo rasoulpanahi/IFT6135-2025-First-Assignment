f6a1506d3d1428aea1c70e8402c2078a3111cdf6
Rasoul Panahi <rasoulpanahi.hb@gmail.com>
2025-03-11 10:45:43 -0400

    one change for mlp code

--- HW1_2025/assignment1_release/mlp.py
+++ HW1_2025/assignment1_release/mlp.py
@@ -3,38 +3,19 @@ from typing import List, Tuple
 from torch import nn
 
 class Linear(nn.Module):
-    r"""Applies a linear transformation to the incoming data: :math:`y = xA^T + b`
-    Args:
-        in_features: size of each input sample
-        out_features: size of each output sample
-    Shape:
-        - Input: :math:`(*, H_{in})` where :math:`*` means any number of
-          dimensions including none and :math:`H_{in} = \text{in\_features}`.
-        - Output: :math:`(*, H_{out})` where all but the last dimension
-          are the same shape as the input and :math:`H_{out} = \text{out\_features}`.
-       
-        >>> m = nn.Linear(20, 30)
-        >>> input = torch.randn(128, 20)
-        >>> output = m(input)
-        >>> print(output.size())
-        torch.Size([128, 30])
-    """
-    def __init__(self, in_features: int, out_features: int) -> None:
+    def __init__(self, in_features: int, out_features: int):
         super(Linear, self).__init__()
-        raise NotImplementedError
-    
+        self.weight = nn.Parameter(torch.randn(out_features, in_features))  # Weight matrix
+        self.bias   = nn.Parameter(torch.randn(out_features))  # Bias vector
+
     def forward(self, input):
-        """
-            :param input: [bsz, in_features]
-            :return result [bsz, out_features]
-        """
-        raise NotImplementedError
+        return torch.matmul(input, self.weight.T) + self.bias  # Linear transformation
 
 
 class MLP(torch.nn.Module):
     # 20 points
     def __init__(self, input_size: int, hidden_sizes: List[int], num_classes: int, activation: str = "relu"):
-        super(MLP, self).__init__() 
+        super(MLP, self).__init__()
         self.input_size = input_size
         self.hidden_sizes = hidden_sizes
         assert len(hidden_sizes) > 1, "You should at least have one hidden layer"
@@ -42,14 +23,14 @@ class MLP(torch.nn.Module):
         self.activation = activation
         assert activation in ['tanh', 'relu', 'sigmoid'], "Invalid choice of activation"
         self.hidden_layers, self.output_layer = self._build_layers(input_size, hidden_sizes, num_classes)
-        
+
         # Initializaton
         self._initialize_linear_layer(self.output_layer)
         for layer in self.hidden_layers:
             self._initialize_linear_layer(layer)
-    
-    def _build_layers(self, input_size: int, 
-                        hidden_sizes: List[int], 
+
+    def _build_layers(self, input_size: int,
+                        hidden_sizes: List[int],
                         num_classes: int) -> Tuple[nn.ModuleList, nn.Module]:
         """
         Build the layers for MLP. Be ware of handlling corner cases.
@@ -60,23 +41,45 @@ class MLP(torch.nn.Module):
             hidden_layers: nn.ModuleList. Within the list, each item has type nn.Module
             output_layer: nn.Module
         """
-        raise NotImplementedError
-    
+        layers = []
+        in_size = input_size
+        for h_size in hidden_sizes:
+            layers.append(Linear(in_size, h_size))
+            in_size = h_size
+        hidden_layers = nn.ModuleList(layers)
+        output_layer  = Linear(in_size, num_classes)
+        return hidden_layers, output_layer
+
     def activation_fn(self, activation, inputs: torch.Tensor) -> torch.Tensor:
         """ process the inputs through different non-linearity function according to activation name """
-        raise NotImplementedError
-        
+        if activation == 'tanh':
+            return torch.tanh(inputs)
+        elif activation == 'relu':
+            return torch.relu(inputs)
+        elif activation == 'sigmoid':
+            return torch.sigmoid(inputs)
+        else:
+            raise ValueError(f"Invalid activation function: {activation}")
+
+
     def _initialize_linear_layer(self, module: nn.Linear) -> None:
         """ For bias set to zeros. For weights set to glorot normal """
-        raise NotImplementedError
-        
+        nn.init.xavier_normal_(module.weight)
+        nn.init.zeros_(module.bias)
+
+
     def forward(self, images: torch.Tensor) -> torch.Tensor:
         """ Forward images and compute logits.
-        1. The images are first fattened to vectors. 
+        1. The images are first fattened to vectors.
         2. Forward the result to each layer in the self.hidden_layer with activation_fn
         3. Finally forward the result to the output_layer.
-        
+
         :param images: [batch, channels, width, height]
         :return logits: [batch, num_classes]
         """
-        raise NotImplementedError
+        x = images.view(images.size(0), -1)  # Flatten the images
+        for layer in self.hidden_layers:
+            x = self.activation_fn(self.activation, layer(x))
+        logits = self.output_layer(x)
+        return logits
+
