cda357015cbb8d2ee342c6cf35ead6e8509cd68a
Rasoul Panahi <rasoulpanahi.hb@gmail.com>
2025-03-11 13:15:38 -0400

    resnet is modified

--- HW1_2025/assignment1_release/resnet18.py
+++ HW1_2025/assignment1_release/resnet18.py
@@ -18,21 +18,21 @@ class BasicBlock(nn.Module):
             :param stride: The stride of first conv
         """
         super(BasicBlock, self).__init__()
-        # Uncomment the following lines, replace the ? with correct values.
-        #self.conv1 = nn.Conv2d(
-        #    ?, planes, kernel_size=3, stride=?, padding=1, bias=False)
-        #self.bn1 = nn.BatchNorm2d(?)
-        #self.conv2 = nn.Conv2d(planes, ?, kernel_size=3,
-        #                       stride=1, padding=1, bias=False)
-        #self.bn2 = nn.BatchNorm2d(?)
+        
+        self.conv1 = nn.Conv2d(
+           ?, planes, kernel_size=3, stride=?, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(?)
+        self.conv2 = nn.Conv2d(planes, ?, kernel_size=3,
+                              stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(?)
 
-        #self.shortcut = nn.Sequential()
-        #if stride != 1 or in_planes != planes:
-        #    self.shortcut = nn.Sequential(
-        #        nn.Conv2d(?, ?,
-        #                  kernel_size=1, stride=stride, bias=False),
-        #        nn.BatchNorm2d(?)
-        #    )
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != planes:
+           self.shortcut = nn.Sequential(
+               nn.Conv2d(?, ?,
+                         kernel_size=1, stride=stride, bias=False),
+               nn.BatchNorm2d(?)
+           )
 
     def forward(self, x):
         # 1. Go through conv1, bn1, relu
@@ -45,14 +45,14 @@ class ResNet18(nn.Module):
     def __init__(self, num_classes=10):
         super(ResNet18, self).__init__()
         # Uncomment the following lines and replace the ? with correct values
-        #self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
-        #                       stride=1, padding=1, bias=False)
-        #self.bn1 = nn.BatchNorm2d(?)
-        #self.layer1 = self._make_layer(64, 64, stride=1)
-        #self.layer2 = self._make_layer(?, 128, stride=2)
-        #self.layer3 = self._make_layer(?, 256, stride=2)
-        #self.layer4 = self._make_layer(?, 512, stride=2)
-        #self.linear = nn.Linear(?, num_classes)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
+                              stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(?)
+        self.layer1 = self._make_layer(64, 64, stride=1)
+        self.layer2 = self._make_layer(?, 128, stride=2)
+        self.layer3 = self._make_layer(?, 256, stride=2)
+        self.layer4 = self._make_layer(?, 512, stride=2)
+        self.linear = nn.Linear(?, num_classes)
 
     def _make_layer(self, in_planes, planes, stride):
         layers = []
